e6caf806fb05620beccad533cb05eb7a37831099
iqbalfn <iqbalfawz@gmail.com>
2017-08-10 17:32:58 +0700

    Add global site name

--- modules/site-meta/_db/install.sql
+++ modules/site-meta/_db/install.sql
@@ -1,6 +1,8 @@
 INSERT IGNORE INTO `site_param` ( `name`, `type`, `group`, `value` ) VALUES
     ( 'facebook_js_tag',                4, 'Third-Party JS',    '' ),
     
+    ( 'site_name',                      1, 'Global', '' ),
+    
     ( 'alexa_analytics_account',        1, 'Code', '' ),
     ( 'alexa_analytics_domain',         1, 'Code', '' ),
     ( 'alexa_site_verification',        1, 'Code', '' ),
